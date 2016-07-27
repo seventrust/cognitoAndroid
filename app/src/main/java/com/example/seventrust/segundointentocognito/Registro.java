66e0b1c3c312c6014291f237098227b22667e080
Yerffrey <yromero@turbus.cl>
2016-07-27 18:57:46 -0400

    Primera entrega de la Saga V 0.1 (alpha)

---
+++ app/src/main/java/com/example/seventrust/segundointentocognito/Registro.java
@@ -0,0 +1,23 @@
+package com.example.seventrust.segundointentocognito;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by seventrust on 27-07-16.
+ */
+public class Registro extends AppCompatActivity{
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registro_usuario_layout);
+
+
+
+
+    }
+}
